5b15e7f5202133dc6fccd63b3bb59c227f6b86ae
杨佳乐 <jlyang1999@163.com>
2020-02-19 16:23:54 +0800

    关于我们样式更改

--- src/views/client/About.vue
+++ src/views/client/About.vue
@@ -1,10 +1,49 @@
 <template>
     <div class="about">
         <Header />
-            关于我们
+            <div class="cardsbox">
+            <div class="mdui-col-sm-6 mdui-col-md-4">
+                <div class="mdui-card">
+                    <div class="mdui-card-media">
+                        <img src="../../assets/images/card.jpg"/>
+                        <div class="mdui-card-media-covered">
+                        <div class="mdui-card-primary">
+                            <div class="mdui-card-primary-title">杨佳乐</div>
+                            <div class="mdui-card-primary-subtitle">20174141007</div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mdui-col-sm-6 mdui-col-md-4">
+                <div class="mdui-card">
+                    <div class="mdui-card-media">
+                        <img src="../../assets/images/card.jpg"/>
+                        <div class="mdui-card-media-covered">
+                        <div class="mdui-card-primary">
+                            <div class="mdui-card-primary-title">王连杰</div>
+                            <div class="mdui-card-primary-subtitle">20174141008</div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         <Bottom />
     </div>
 </template>
+
+<style>
+    .about {
+        background: linear-gradient(190deg, #000, #191654);
+    }
+    .cardsbox {
+        width: 100%;
+        display: flex;
+        padding: 60px 10%;
+        justify-content: space-around;
+    }
+</style>
 
 <script>
 import Header from './Header'
